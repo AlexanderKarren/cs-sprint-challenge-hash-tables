415e1a549fc2bf819eeb9fb066c166d17f427b04
Alexander Karren <alex@karren.com>
2020-08-07 11:32:50 -0600

    completed ex4.py

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -15,17 +15,17 @@ def intersection(arrays):
             
 
     # print(num_hash)
-    return sorted(result)
+    return result
 
 
 if __name__ == "__main__":
-    # arrays = []
+    arrays = []
 
-    # arrays.append(list(range(1000000, 2000000)) + [1, 2, 3])
-    # arrays.append(list(range(2000000, 3000000)) + [1, 2, 3])
-    # arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
+    arrays.append(list(range(1000000, 2000000)) + [1, 2, 3])
+    arrays.append(list(range(2000000, 3000000)) + [1, 2, 3])
+    arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
 
-    # print(intersection(arrays))
+    print(intersection(arrays))
 
     arrays = [
     [1, 2, 3, 4, 5],
--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -2,10 +2,24 @@ def has_negatives(a):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    negative_hash = {}
+    result = []
+    for i in range(len(a)):
+        # Number is negative
+        if negative_hash.get(abs(a[i])) is not None:
+            if a[i] < 0 and negative_hash.get(abs(a[i])) is True:
+                result.append(abs(a[i]))
+            elif a[i] > 0 and negative_hash.get(abs(a[i])) is False:
+                result.append(abs(a[i]))
+        else:
+            if a[i] < 0:
+                negative_hash.update({abs(a[i]): False})
+            else:
+                negative_hash.update({abs(a[i]): True})
 
     return result
 
 
 if __name__ == "__main__":
+    # Output should be: [1, 2, 4]
     print(has_negatives([-1, -2, 1, 2, 3, 4, -4]))
